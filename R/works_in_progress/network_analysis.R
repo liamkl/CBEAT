3bf9e038d311fe033f0f3ce137e0bc1411e585f5
liam <liam.k.loscalzo@gmail.com>
2017-11-13 09:59:08 -0500

    network analysis update

--- R/works_in_progress/network_analysis.R
+++ R/works_in_progress/network_analysis.R
@@ -1,22 +1,25 @@
 library(igraph)
+library(dplyr)
 
 nodes <- nodes[!duplicated(nodes$bioguideId), ]
-net <- graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
-net <- simplify(net, edge.attr.comb = list(weight = "sum", "ignore"))
 
-V(net)$size <- 8
+nodes.senate <- filter(nodes, chamber_id == 2)
+edges.senate <- filter(edges, 
+       from %in% nodes.senate$bioguideId,
+       to %in% nodes.senate$bioguideId)
 
-V(net)$frame.color <- "white"
+net.sen <- graph_from_data_frame(d = edges.senate, 
+                                    vertices = nodes.senate,
+                                    directed = TRUE)
+net.sen <- simplify(net.sen, edge.attr.comb = list(weight = "sum", "ignore"))
+net.senate <- delete.edges(net.sen, E(net.sen)[weight < quantile(E(net.sen)$weight, 0.90)])
+colrs <- c("D" = "blue", "I" = "green", "R" = "red")
+V(net.senate)$color <- colrs[V(net.senate)$party]
+V(net.senate)$size <- 4
+V(net.senate)$label <- ""
+E(net.senate)$arrow.size <- 0.1
+E(net.senate)$width <- E(net.senate)$weight / 50
+plot(net.senate, layout = layout_with_kk)
 
-V(net)$color <- "orange"
-
-V(net)$label <- ""
-E(net)$width
-
-
-mean(E(net)$weight)
-quantile(E(net)$weight, 0.80)
-
-cutoff <- mean(E(net)$weight)
-net.sp <- delete_edges(net, E(net)[weight < cutoff])
-plot(net.sp, layout = layout_in_circle)
+degree.distribution(net.senate) %>% hist()
+     
