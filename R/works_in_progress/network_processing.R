65ac863f8ba3e7b44b23e9a1f4dc071fb485c9b3
liam <liam.k.loscalzo@gmail.com>
2017-12-10 18:07:53 -0500

    clean up network processing

--- R/works_in_progress/network_processing.R
+++ R/works_in_progress/network_processing.R
@@ -1,26 +1,12 @@
 library(igraph)
 library(tidyverse)
 
-nodes <- read.csv("./data/output_data/nodes.csv")
-edges <- read.csv("./data/output_data/edges.csv")
+nodes <- read.csv("./rawdata/output_data/nodes.csv")
+edges <- read.csv("./rawdata/output_data/edges.csv")
 
 G <- graph_from_data_frame(d = edges, vertices = nodes, directed = TRUE)
 G <- simplify(G, edge.attr.comb = list(weight = "sum", "ignore"))
-G_red <- delete.edges(G, E(G)[weight < quantile(E(G)$weight, 0.90)])
-
-colrs <- c("D" = "blue", "I" = "green", "R" = "red")
-# V(G_red)$color <- colrs[V(G_red)$party]
-# V(G_red)$size <- 4
-# V(G_red)$label <- ""
-# E(G_red)$arrow.size <- 0.1
-# E(G_red)$width <- E(G_red)$weight / 50
-# plot(G, layout = layout_with_kk)
-betweenness(G) %>% View
-
-# Calculate in and out degree, add to nodes DF
-deg_out <- degree(G, mode = "out")
-deg_in <- degree(G, mode = "in")
-nodes <- nodes %>% cbind(deg_out, deg_in)
+simp_edges <- as_data_frame(G, what = "edges")
 
 total_in <- strength(G, mode = "in", weights = E(G)$weight)
 total_out <- strength(G, mode = "out", weights = E(G)$weight)
@@ -33,7 +19,6 @@ total_in.d <- data.frame(bioguideId = V(D)$name,
 total_out.d <- data.frame(bioguideId = V(D)$name,
                             total_out.d = strength(D, mode = "out", weights = E(D)$weight))
 nodes <- nodes %>% left_join(total_in.d) %>% left_join(total_out.d)
-  
   
 R <- induced_subgraph(G, V(G)$party == "R")
 total_in.r <- data.frame(bioguideId = V(R)$name,
@@ -49,5 +34,6 @@ nodes <- nodes %>% mutate(total_in_nonparty = total_in - total_in_party,
                           total_out_nonparty = total_out - total_out_party) %>%
   filter(bioguideId != "K000148") # Filter out John Kerry
 
-# 
+
 write_csv(nodes, paste0(getwd(), "/data/output_data/nodes_proc.csv"))
+write_csv(simp_edges, paste0(getwd(), "/data/output_data/simplified_edges.csv"))
